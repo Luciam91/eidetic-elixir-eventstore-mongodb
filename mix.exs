fc02a95d49811b9be8e74fb45605e75c9a171ff3
David McKay <david@rawkode.com>
2017-04-13 10:50:34 +0100

    Version bump to 0.1.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Eidetic.EventStore.MongoDB.Mixfile do
 
   def project do
     [app: :eidetic_eventstore_mongodb,
-      version: "0.0.8",
+      version: "0.1.0",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
